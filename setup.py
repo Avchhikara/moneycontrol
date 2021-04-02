07fa820b96ebe142ca76e80e6ae0d0f014ac2f8e
Avnish <Avnishway@gmail.com>
2021-04-03 04:03:21 +0530

    fixed one bug so changed the version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "readme.md").read_text()
 
 setup(
     name="moneycontrol",
-    version="1.0.0",
+    version="1.0.1",
     description="Web scraper for moneycontrol.com",
     long_description=README,
     long_description_content_type="text/markdown",
